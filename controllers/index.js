afe262937c3cbf490137b58968efde1122db2676
Vir-Freddy <38891527+Vir-Freddy@users.noreply.github.com>
2020-11-10 21:44:51 -0500

    add post function to the api, with auto increase of id #s

--- controllers/index.js
+++ controllers/index.js
@@ -5,11 +5,29 @@ const getAllTeams = (request, response) => {
 }
 
 const getTeamById = (request, response) => {
-  const {id} = request.params
+  const { id } = request.params
 
   const foundTeam = teams.filter((team) => team.id === parseInt(id))
-  
+
   return response.send(foundTeam)
 }
+const saveNewTeam = (request, response) => {
+  const {
+    id, location, mascot, abbreviation, conference, division
+  } = request.body
 
-module.exports = {getAllTeams, getTeamById}
+  if (!location || !mascot || !abbreviation || !conference || !division) {
+    return response.status(400).send('All fields required: location, mascot, abbreviation, conference, division')
+  }
+  const newTeam = {
+    id, location, mascot, abbreviation, conference, division
+  }
+
+  newTeam.id = teams.length + 1
+
+  teams.push(newTeam)
+
+  return response.status(201).send(newTeam)
+}
+
+module.exports = { getAllTeams, getTeamById, saveNewTeam }
--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 const express = require('express')
-const {getAllTeams, getTeamById} = require('./controllers/index')
+const bodyParser = require('body-parser')
+const { getAllTeams, getTeamById, saveNewTeam } = require('./controllers/index')
 
 const app = express()
 
 app.get('/teams', getAllTeams)
 
 app.get('/teams/:id', getTeamById)
+
+app.post('/teams', bodyParser.json(), saveNewTeam)
 
 app.listen(1337, () => {
   console.log('Listening on port 1337...') // eslint-disable-line no-console
